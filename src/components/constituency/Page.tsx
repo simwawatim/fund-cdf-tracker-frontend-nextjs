5f15e08bf262de73d7fca0be278aa47e1a1d0c4e
tim <simwawatimothy64@gmail.com>
2025-10-31 11:30:02 +0200

    fix

--- src/components/constituency/Page.tsx
+++ src/components/constituency/Page.tsx
@@ -1,22 +1,24 @@
+import { useState } from "react";
 import ConstituencyTable from "@/components/constituency/Table";
 import HeaderPage from "@/components/header/Header";
 import Sidebar from "@/components/sidebars/SideBar";
 
 const ConstituencyPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
   return (
     <div className="flex flex-col h-screen">
       {/* Header */}
-      <HeaderPage />
+      <HeaderPage isOpen={isOpen} toggleSidebar={toggleSidebar} />
 
       {/* Main content area */}
       <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar (hidden on mobile) */}
-        <aside className="hidden md:block md:w-64 overflow-y-auto bg-gray-50 border-r">
-          <Sidebar />
-        </aside>
+        {/* Sidebar */}
+        <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
 
         {/* Main Content */}
-        <main className="flex-1 p-4 md:p-6 overflow-y-auto bg-white">
+        <main className="flex-1 p-4 md:p-6 overflow-y-auto bg-white lg:ml-[250px]">
           <ConstituencyTable />
         </main>
       </div>
